39946158534ab57962bc2406b33da2a33ff2e1a7
alex_nemynov <alexnemynov@yandex.ru>
2024-11-10 15:34:31 +0300

    Added user registartion & password hashing

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -6,14 +6,18 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\PreUpdate;
 use Doctrine\ORM\Mapping\Table;
 
-#[Entity, Table(name: 'users')]
+#[Entity, Table(name: 'users'), HasLifecycleCallbacks]
 class User
 {
     #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
@@ -44,6 +48,16 @@ class User
     {
         $this->categories = new ArrayCollection();
         $this->transactions = new ArrayCollection();
+    }
+
+    #[PrePersist, PreUpdate]
+    public function updateTimestamps(LifecycleEventArgs $args): void
+    {
+        if (! isset($this->createdAt)) {
+            $this->createdAt = new \DateTime();
+        }
+
+        $this->updatedAt = new \DateTime();
     }
 
     public function getId(): int
