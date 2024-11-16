340cda50b45e665c63465063b485a51cb1f80a7f
alex_nemynov <alexnemynov@yandex.ru>
2024-11-16 16:01:24 +0300

    Implement Session Interface

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use App\Contracts\AuthInterface;
-use App\Contracts\SessionInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -16,17 +15,16 @@ class AuthMiddleware implements MiddlewareInterface
 {
     public function __construct(
         private readonly ResponseFactoryInterface $responseFactory,
-        private readonly SessionInterface $session,
         private readonly AuthInterface $auth
     ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (empty($this->session->get('user'))) {
-            return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
+        if ($user = $this->auth->user()) {
+            return $handler->handle($request->withAttribute('user', $user));
         }
 
-        return $handler->handle($request->withAttribute('user', $this->auth->user()));
+        return $this->responseFactory->createResponse(302)->withHeader('Location', '/login');
     }
 }
