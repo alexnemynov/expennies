1a25fb1aac1ca1d3e1fa517c88b11c8cd3f49a6b
alex_nemynov <alexnemynov@yandex.ru>
2024-11-23 18:36:25 +0300

    POST Ajax Requests With CSRF Fields

--- app/ResponseFormatter.php
+++ app/ResponseFormatter.php
@@ -12,8 +12,7 @@ class ResponseFormatter
         ResponseInterface $response,
         mixed $data,
         int $flags = JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_THROW_ON_ERROR
-    ): ResponseInterface
-    {
+    ): ResponseInterface {
         $response = $response->withHeader('Content-Type', 'application/json');
 
         $response->getBody()->write(json_encode($data, $flags));
--- configs/middleware.php
+++ configs/middleware.php
@@ -9,6 +9,7 @@ use App\Middleware\StartSessionMiddleware;
 use App\Middleware\ValidationErrorsMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
+use Slim\Middleware\BodyParsingMiddleware;
 use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -25,6 +26,7 @@ return function (App $app) {
     $app->add(ValidationErrorsMiddleware::class);
     $app->add(OldFormDataMiddleware::class);
     $app->add(StartSessionMiddleware::class);
+    $app->add(BodyParsingMiddleware::class);
 
     // Logger
     $app->addErrorMiddleware(
