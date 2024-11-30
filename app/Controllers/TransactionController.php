91064834d17b94492583548289e07d91da5b0b5b
alex_nemynov <alexnemynov@yandex.ru>
2024-11-30 18:49:48 +0300

    CRUD for Transactions - some refactoring

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -18,7 +18,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
-class TransactionsController
+class TransactionController
 {
     public function __construct(
         private readonly Twig                             $twig,
